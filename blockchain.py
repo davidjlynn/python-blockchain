7385c51ffb99f304a7011fbc34f9b77def93a1f6
David James Lynn <david.james.lynn@gmail.com>
2017-12-28 03:14:57 +0000

    Add specifying port via parameter

--- blockchain.py
+++ blockchain.py
@@ -1,6 +1,6 @@
 import hashlib
 import json
-from textwrap import dedent
+
 from time import time
 from uuid import uuid4
 from flask import Flask, jsonify, request
@@ -279,4 +279,11 @@ def consensus():
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    from argparse import ArgumentParser
+    
+    parser = ArgumentParser()
+    parser.add_argument('-p', '--port', default=5000, type=int, help='port to listen on')
+    args = parser.parse_args()
+    port = args.port
+
+    app.run(host='0.0.0.0', port=port)
